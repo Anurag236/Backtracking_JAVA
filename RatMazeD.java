bca07478275107fdaabc56170c34280903c49281
Anurag236 <anuragkumar3260@gmail.com>
2024-03-31 01:14:22 +0530

    commit

--- RatMazeD.java
+++ RatMazeD.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * RatMazeD -> When we have given some Restrictions 
  *             in 2D matrix 
  */
+import java.util.*;
 public class RatMazeD {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
     }   
 }
